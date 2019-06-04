3ea117209e10a79bfe599834e88a5ff135738b40
a532367171 <532367171@qq.com>
2019-06-04 12:52:15 +0800

    添加项目文件。

---
+++ spring.net_Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace spring.net_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            IUserInfo userInfo = (IUserInfo)ctx.GetObject("UserInfo");
+            Console.WriteLine(userInfo.ShowMss());
+            Console.Read();
+            
+        }
+    }
+}
---
+++ spring.net_Demo/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace spring.net_Demo
+{
+    public class UserInfo : IUserInfo
+    {
+        public string ShowMss()
+        {
+            return "Hello Spring.NET";
+        }
+    }
+}
