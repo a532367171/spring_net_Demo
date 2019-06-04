9798920cacaeccce2e80b0d3ac02c7ab335d0bcd
a532367171 <532367171@qq.com>
2019-06-04 12:57:46 +0800

    19.06.04.02.1

--- spring.net_Demo/Program.cs
+++ spring.net_Demo/Program.cs
@@ -11,6 +11,7 @@ namespace spring.net_Demo
     {
         static void Main(string[] args)
         {
+
             IApplicationContext ctx = ContextRegistry.GetContext();
 
             IUserInfo userInfo = (IUserInfo)ctx.GetObject("UserInfo");
